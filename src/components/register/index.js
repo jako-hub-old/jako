1fa2045fbcd488228d6b0119e8167e4205b89ad7
Jorge Alejandro Quiroz Serna <jakop.box@gmail.com>
2019-03-19 18:18:19 -0500

    Se implementa componente de carga en registro

--- src/components/register/index.js
+++ src/components/register/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import RegisterForm from "./RegisterForm";
+import { withLoader } from '../../providers';
 
+/**
+ * This component allows to handle the user register
+ * @author Jorge Alejandro Quiroz Serna <jakop.box@gmail.com>
+ * @class RegisterComponent
+ * @extends {React.Component}
+ */
 class RegisterComponent extends React.Component {
     state = {
         form : {
@@ -30,6 +37,10 @@ class RegisterComponent extends React.Component {
         this.props.navigation.navigate("Login");
     }
 
+    onRegister() {
+        
+    };
+
     render() {
         const {
             form,
@@ -38,11 +49,12 @@ class RegisterComponent extends React.Component {
 
         return (
             <RegisterForm
-                form        = {form}
-                onChange    = {this.onChange.bind(this)}
-                goToLogin   = {() => this.goToLogin()}
-                togglePassword  = {() => this.togglePassword()}
-                viewPassword    = {viewPassword}
+                form            = { form                        }
+                onChange        = { this.onChange.bind(this)    }
+                goToLogin       = { () => this.goToLogin()      }
+                togglePassword  = { () => this.togglePassword() }
+                viewPassword    = { viewPassword                }
+                onSubmit        = { () => this.onRegister()     }
             />
         )
     }
@@ -52,4 +64,10 @@ RegisterComponent.propTypes = {
     navigation : PropTypes.object.isRequired,
 };
 
-export default RegisterComponent;
+RegisterComponent.propTypes = {
+    startLoading    : PropTypes.func,
+    stopLoading     : PropTypes.func,
+    loading         : PropTypes.bool,
+};
+
+export default withLoader(RegisterComponent);   
--- src/providers/withLoader.js
+++ src/providers/withLoader.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { startLoading, stopLoading } from '../store/actions/global.actions';
 import { bindActionCreators } from 'redux';
+import PropTypes from 'prop-types';
 
 const mapDispatchToProps = dispatch => bindActionCreators({
     startLoading,
@@ -12,10 +13,20 @@ const mapStateToProps = ({global:{loadingState}}) => ({
     loading : loadingState,
 });
 
-export default () => WrappedComponent => (connect(mapStateToProps, mapDispatchToProps)(
-    props => (
-        <WrappedComponent 
-            {...props}
-        />
-    )
+export const propTypes = {
+    startLoading    : PropTypes.func,
+    stopLoading     : PropTypes.func,
+    loading         : PropTypes.bool,
+};
+
+export default WrappedComponent => (connect(mapStateToProps, mapDispatchToProps)(
+    class extends React.PureComponent {
+        render() {
+            return (
+                <WrappedComponent 
+                    {...this.props}
+                />
+            )
+        }
+    }
 ));

f6031d9618d64c60e76e93f10f98691813ce1bef
Jorge Alejandro Quiroz Serna <jakop.box@gmail.com>
2019-05-07 16:32:39 -0500

    fixed navigator

--- src/configs/navigators.js
+++ src/configs/navigators.js
@@ -68,7 +68,7 @@ const MainNavigator = createBottomTabNavigator({
     ContactTab  : createTabOptions(ContactNavigator, 'question-circle'),
 }, {
     shifting            : true,
-    initialRouteName    : "MyGamesTab",
+    initialRouteName    : "HomeTab",
     activeColor         : "white",
     labeled             : false,
     barStyle : {
